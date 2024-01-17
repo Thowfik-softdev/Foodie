18e7f285b8b4eca5d662f8e58944e47e85edbddd
Thowfik-mtj3 <thowfikmtj3@gmail.com>
2024-01-17 10:12:58 +0530

    First Commit

---
+++ Home.js
@@ -0,0 +1,10 @@
+$('.main-carousel').flickity({
+    cellAlign: 'center',
+    contain: true,
+    dragThreshold: 10,
+    freeScroll: true,
+    wrapAround: false,
+    groupCells: true,
+    prevNextButtons: false
+  });
+
